b34da5ee9e35f14d0482405d3de777f7a5c8cdba
TynClause <elvinnurfurqon17@gmail.com>
2020-11-10 18:43:44 +0700

    update tentang kami

--- lib/screens/tentang_kami_screen.dart
+++ lib/screens/tentang_kami_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TentangKami extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("mantap gan"),
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 24.0,
+              top: 25.0,
+            ),
+            child: Text(
+              "Tentang Kami",
+              style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w600,
+                  fontStyle: FontStyle.normal,
+                  color: Color(0xFF333333),
+                  fontSize: 24.0),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
