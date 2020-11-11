52f807aaacf3b5e7dd001988b726c01e6567a9d6
TynClause <elvinnurfurqon17@gmail.com>
2020-11-12 00:06:52 +0700

    update tentang kami screen

--- lib/screens/tentang_kami_screen.dart
+++ lib/screens/tentang_kami_screen.dart
@@ -7,19 +7,45 @@ class TentangKami extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: 24.0,
-              top: 25.0,
-            ),
-            child: Text(
-              "Tentang Kami",
-              style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w600,
-                  fontStyle: FontStyle.normal,
-                  color: Color(0xFF333333),
-                  fontSize: 24.0),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 24.0,
+                  top: 25.0,
+                  bottom: 7.0,
+                ),
+                child: Text(
+                  "Tentang Kami",
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600,
+                      fontStyle: FontStyle.normal,
+                      color: Color(0xFF333333),
+                      fontSize: 24.0),
+                ),
+              ),
+              Image.asset(
+                'assets/maps.png',
+                width: 390.0,
+                height: 219.0,
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 59.0,
+                  left: 24.0,
+                ),
+                child: Text(
+                  "Mantap",
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600,
+                      fontStyle: FontStyle.normal,
+                      color: Color(0xFF2962FF),
+                      fontSize: 14.0),
+                ),
+              ),
+            ],
           ),
         ),
       ),
